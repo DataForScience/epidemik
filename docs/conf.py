a42b0b38d1391e3255aab21cf1e87eea29d95a6c
Bruno Goncalves <bgoncalves@gmail.com>
2024-04-15 19:32:06 -0400

    update doc requirements

--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,9 @@ release = '0.0.20'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx_rtd_theme',
+]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
